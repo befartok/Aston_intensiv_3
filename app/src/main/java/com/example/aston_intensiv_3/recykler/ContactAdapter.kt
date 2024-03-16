abee4a58c2b69abb6f36cbe227dba99e570212ac
befartok <ancherkasoff@gmail.com>
2024-03-16 22:16:58 +0400

    remove todo

--- app/src/main/java/com/example/aston_intensiv_3/recykler/ContactAdapter.kt
+++ app/src/main/java/com/example/aston_intensiv_3/recykler/ContactAdapter.kt
@@ -28,7 +28,6 @@ class ContactAdapter(
 
         clickedPosition = holder.adapterPosition
 
-// TODO: убрать тосты
         binding.root.setOnClickListener {
 
             clickedPosition = holder.adapterPosition
